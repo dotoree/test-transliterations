adc7ccf0631d1362f08c65929cdd1ad39245ff81
dotoree <vdotoree@yahoo.gr>
2024-12-30 16:02:31 +0200

    Add validator and more

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.23.4
 require (
 	github.com/mozillazg/go-unidecode v0.2.0
 	github.com/stts-se/translit v0.0.0-20230223122049-5a73aa90dfd9
+	github.com/wagslane/go-password-validator v0.3.0
+	golang.org/x/text v0.21.0
 	gorm.io/driver/sqlite v1.5.7
 	gorm.io/gorm v1.25.12
 )
@@ -13,5 +15,4 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
-	golang.org/x/text v0.21.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
+	"strconv"
 	"strings"
 
 	"github.com/dotoree/test-transliterations/storage"
 	"github.com/dotoree/test-transliterations/utils"
+	passwordvalidator "github.com/wagslane/go-password-validator"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 	"gorm.io/gorm"
 )
 
@@ -31,14 +36,28 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("")
 	p := []string{}
 	for _, word := range *words {
 		fmt.Printf("%#v\n", word)
-		p = append(p, word.LatinWord)
+		w := word.LatinWord
+		p = append(p, w)
 	}
-	fmt.Println("\n" + strings.Join(p[:], "-"))
-	fmt.Println("")
+
+	// Add number
+	p = append(p, strconv.Itoa(rand.IntN(1000)+1))
+
+	// Add caser for title case first word
+	caser := cases.Title(language.English)
+	p[0] = caser.String(p[0])
+
+	// Join
+	password := strings.Join(p[:], "-")
+
+	// Validate
+	entropy := passwordvalidator.GetEntropy(password)
+
+	fmt.Printf("\n\nPassword: %s\n", password)
+	fmt.Printf("Entropy: %v\n\n", entropy)
 }
 
 func importGreek() {
